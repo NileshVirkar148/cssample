ab124858340bbbbf50cd55a9484dbe566b5c2897
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2025-05-27 19:09:57 +0530

    Update FloatComparisonExample.cs

--- src/FloatComparisonExample.cs
+++ src/FloatComparisonExample.cs
@@ -1,1 +1,19 @@
+using System;
 
+class FloatComparisonExample
+{
+    public void CompareFloats()
+    {
+        float a = 0.1f + 0.2f;
+        float b = 0.3f;
+
+        if (a == b)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not Equal");
+        }
+    }
+}
